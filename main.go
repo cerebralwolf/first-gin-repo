88662bb25f0bf736fd686870783c24ff36bc78de
cerebralwolf <jiki@cerebralwolf.com>
2024-12-27 00:16:52 +0000

    initial first-steps commit

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+func wolfHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "software development in go",
+	})
+}
+
 func main() {
 	router := gin.Default()
-	router.GET("/wolf", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "software development in go",
-		})
-	})
+	router.GET("/wolf", wolfHandler)
 	router.Run(":3010")
 }
